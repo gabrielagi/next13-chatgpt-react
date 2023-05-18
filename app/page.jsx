0a0c32f735a6750586e3197f8eba39f2d3b4f662
gabrielagi <gabrielagi503@gmail.com>
2023-05-18 16:42:26 -0300

    Agregando contenido a Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
 export const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Descubre y comparte
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">Prompsai es una herramienta de avisos alimentada por IA para lograr
+      descubrir, crear y compartir resoluciones o ideas creativas</p>
+    </section>
+    /* FEED */
   )
 }
 
