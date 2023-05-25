02786c5c625bbc313c87481013ad5a0202e05c78
gabrielagi <gabrielagi503@gmail.com>
2023-05-25 16:54:16 -0300

    Seteo de Login

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,10 +12,12 @@ const Nav = () => {
 
   useEffect (() => {
     const setProviders = async () => {
-    const response = await getProviders();
+      const response = await getProviders();
+      setProviders(response);
   }
+    {/*Esto me va a permitir loguearme usando Google o next-auth*/}
 
-  setProviders(response);
+  setProviders();
   }, [])
 
   return (
@@ -31,7 +33,7 @@ const Nav = () => {
         <p className="logo-text">Prompsai</p>
       </Link>
 
-      {/*Mobile Navigation*/}
+      {/*Desktop Navigation*/}
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
           <div className="flex gap-3 md:gap-5">
@@ -55,8 +57,49 @@ const Nav = () => {
           </div>
         ) : (
           <> 
+          {providers && Object.values(providers).map((provider) => (
+            <button
+            type="button"
+            key={provider.name}
+            onClick={() => signIn(provider.id)}
+            className="black_btn"
+            >
+              Sign In
+            </button>
+          ))}
           </>
         )}
+      </div>
+
+      {/*Mobile Navigation*/}
+
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+                //onClick={() => ()}
+              />
+          </div>
+        ) : (
+<> 
+          {providers && Object.values(providers).map((provider) => (
+            <button
+            type="button"
+            key={provider.name}
+            onClick={() => signIn(provider.id)}
+            className="black_btn"
+            >
+              Sign In
+            </button>
+          ))}
+          </>
+        )}
+
       </div>
     </nav>
   )
