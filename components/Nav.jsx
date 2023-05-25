c485ee4f8485f276a9f142fd9d3fd668fd870853
gabrielagi <gabrielagi503@gmail.com>
2023-05-25 12:59:02 -0300

    setProviders

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,17 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
   const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+
+  useEffect (() => {
+    const setProviders = async () => {
+    const response = await getProviders();
+  }
+
+  setProviders(response);
+  }, [])
+
   return (
     <nav className="felx-between w-full mb-16 pt-3">
       <Link href='/' className="flex gap-2 flex-center">
@@ -25,7 +36,7 @@ const Nav = () => {
         {isUserLoggedIn ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
-              Create <Post></Post>
+              Create Post
             </Link>
 
             <button type="button" onClick={signOut} className="outline_btn">
@@ -34,7 +45,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/mages/profile.svg"
+                src="/assets/images/logo.svg"
                 width={37}
                 height={37}
                 className="rounded-full"
