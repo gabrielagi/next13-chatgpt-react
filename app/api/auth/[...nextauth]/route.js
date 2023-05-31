899ad7bfcd5a2f6259fabd0d6041816d579ceccd
gabrielagi <gabrielagi503@gmail.com>
2023-05-31 17:09:45 -0300

    mongoose google-auth conf

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,25 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
+import { connectToDB } from '@utils/database'
+
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: '',
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
     async session({ session }) {
 
     },
     async signIn({ profile }) {
-        
+        try {
+            await connectToDB();
+        } catch (error) {
+            
+        }
     }
 })
 
