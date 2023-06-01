c6a6a8bed687b151b58670ba91bdcd31b1add1dd
gabrielagi <gabrielagi503@gmail.com>
2023-06-01 13:07:22 -0300

    fix eroor with the DB connection

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,38 +11,40 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
-    async session({ session }) {
-        const sessionUser =  await User.findOne ({
-            email: session.user.email
-        })
+    callbacks: {
+        async session({ session }) {
+            const sessionUser =  await User.findOne ({
+                email: session.user.email
+            })
 
-        session.user.id = sessionUser._id.toSting();
-        // knowing wich user is currently online
-        return session;
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
+            session.user.id = sessionUser._id.toSting();
+            // knowing wich user is currently online
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
 
-        // check if a user already exists
-            const userExist = await User.findOne({
-                email: profile.email
-            });
-            
-        // if not, create a new user
-            if(!userExist) {
-                await User.create ({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").tOLowerCase(),
-                    image: profile.picture
-                })
+            // check if a user already exists
+                const userExist = await User.findOne({
+                    email: profile.email
+                });
+                
+            // if not, create a new user
+                if(!userExist) {
+                    await User.create ({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").tOLowerCase(),
+                        image: profile.picture
+                    })
+                }
+
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-
-        } catch (error) {
-            console.log(error);
-            return false;
         }
     }
-})
+}) 
 
 export { handler as GET, handler as POST }
